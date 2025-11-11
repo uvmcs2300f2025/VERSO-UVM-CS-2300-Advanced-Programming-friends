e0a7e1ccdd50151ff0828b9a187e7ff1c29cc3cf
EllieSheppard2 <elliesheppard213@gmail.com>
2025-11-11 13:45:01 -0500

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,3 +1,4 @@
+## Bugs added: ES
 with open("numbers.html", "w") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
