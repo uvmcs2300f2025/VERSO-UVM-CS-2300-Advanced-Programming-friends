66802e69773340b025ba483d3454079f38dd7725
jsparr72 <sparrjack72@gmail.com>
2025-11-11 14:14:08 -0500

    bugfixes in remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -10,7 +10,7 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     sort(arr.begin(), arr.end());
     int n = arr.size();
     for(int i=0; i<n; i++){
-        if(i == 0 || arr[i+1] != arr[i-1]){
+        if(i == 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
         else{
@@ -37,7 +37,7 @@ int main(){
     cout << endl;
     cout << "Removed elements: ";
     for(int i=0; i<removed_elements.size(); i++){
-        cout << removed_elements[i] << " hewo";
+        cout << removed_elements[i] << " ";
     }
     cout << endl;
     return 0;
