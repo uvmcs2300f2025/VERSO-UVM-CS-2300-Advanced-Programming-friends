f91a8c7b19c33cc7cca5deb34e45780351a5c62b
Zachary Elkin <156862553+zbelkin16@users.noreply.github.com>
2025-11-11 13:48:48 -0500

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,14 +1,14 @@
 #TV
-
+#Bugs by ZE
 #We should add hex_to_rgb
 
-def rgb_to_hex(r, g, b):
+def rgb_to_hex(r, b, g):
     #Convert values piece-wise
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
     #Format them as hex
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    return '{:02X}{:02X}{:02X}'.format(r, b, g)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
