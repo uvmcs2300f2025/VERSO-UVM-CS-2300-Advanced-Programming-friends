78512b348ad53ab47484859cd13d549763ebed16
Lucy <125324723+LucyKHart@users.noreply.github.com>
2025-11-11 13:47:36 -0500

    Introduced bugs datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,11 +1,12 @@
+#bugs introduced - LH
 from datetime import datetime
 
 date_str = "2022-03-17 10:45:30"
 
 #parsing string into a datetime object
-date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S') 
+date_obj = datetime.strptime(date_str, '%Y/%m/%d %H::%M::%S') 
 
 #formatting datetime object into mmddyy and hour minute second
-formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
+formatted_date = date_obj.strftime('%Y/%d/%m %H:%M:%s')
 
 print(formatted_date)
