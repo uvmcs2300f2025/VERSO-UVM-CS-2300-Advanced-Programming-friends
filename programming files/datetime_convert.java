3cc02b01413ce40dd2aee952fcbe6e089164e49a
macf2 <cormac.farrell@uvm.edu>
2025-11-11 14:13:22 -0500

    Fix date format in datetime_convert.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -6,9 +6,9 @@ public class datetime_convert {
     public static void main(String[] args) {
         //LH This is the required date format 
         String dateStr = "2022-03-17 10:45:30";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
-        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss"));
+        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
 
         System.out.println(formattedDate);
     }
