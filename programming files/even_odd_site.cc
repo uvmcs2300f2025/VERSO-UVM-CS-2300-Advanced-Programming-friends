6f44028b90c28a6c83080981675612a3706c5fd1
Tov <70763482+tvitols@users.noreply.github.com>
2025-11-11 13:46:46 -0500

    Update even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -1,11 +1,13 @@
+// bugs introduced: TV
+
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main() {
     ofstream file;
-    file.open("numbers.html");
-    file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
+    file.open("lotsofnumbersfromnumbers.html");
+    file << "<html>\n</br><head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
     for (int i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
@@ -15,10 +17,10 @@ int main() {
             file << "<tr><td></td><td>" << i << "</td></tr>\n";
         }
     }
-    file << "</table>\n</body>\n</html>";
     file.close();
+    file << "</table>\n</body>\n</html>";
     ifstream input("numbers.html");
-    cout << input.rdbuf();
     input.close();
+    cout << input.rdbuf();
     return 0;
 }
